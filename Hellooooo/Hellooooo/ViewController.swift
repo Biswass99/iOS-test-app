a5107590b16031e29e8537554489a0b4f19eb994
user150447 <user150447@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-29 14:43:52 -0600

    Added Background Color

--- Hellooooo/Hellooooo/ViewController.swift
+++ Hellooooo/Hellooooo/ViewController.swift
@@ -2,9 +2,10 @@
 //  ViewController.swift
 //  Hellooooo
 //
-//  Created by user150447 on 1/7/19.
+//  Created by Salil Biswas on 1/29/19.
 //  Copyright © 2019 user150447. All rights reserved.
 //
+// This app has a label and a button with background colors of green and red.
 
 import UIKit
 
@@ -15,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+  // when the button is clicked, the text of the label changes to "Hellooooo from Salil !"
     @IBAction func send(_ sender: Any) {
         self.label.text = "Hellooooo from Salil !"
     }
